7b690ba65a3d52d2e97d45cd69c2a0d4890661c6
ALEXEY <minkravter@gmail.com>
2023-10-18 21:07:55 +0300

    AlexeyStart

--- src/main/kotlin/StringsAndNumbers.kt
+++ src/main/kotlin/StringsAndNumbers.kt
@@ -1,9 +1,12 @@
 class StringsAndNumbers {
-    //
-    // /
     val a = "I am the storm that is approaching"
     val b = "Provoking black clouds in isolation"
+    val c = 5
+
     fun execute() {
-        println("...${a}!!!\n${b}!\n...")
+        println("...${a}!!!\n${b}!\n... ${c + c}")
+        println("Строка $a :String")
+        println("Целочисленное $b :Int ")
+
     }
 }
